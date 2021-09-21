4f3eeb352c84c220971e354de70a7d192eccc205
Yo Yehudi <yochannah@gmail.com>
2021-09-21 21:52:33 +0100

    hmm, needs to perturb backwards

--- script.js
+++ script.js
@@ -16,15 +16,60 @@ document.addEventListener("DOMContentLoaded", function() {
     w: seed.width.baseVal.value
   };
 
-  createPolygon(generatePoints(), "#005577");
+  let points = generatePoints(20, middle);
+  for (var i = 0; i < points.length; i++) {
+    createPolygon(points[i], "#005577", middle);
+  }
 
 });
 
-function generatePoints() {
-  return [[30,30],[90,90],[100,20]];
+//from MDN thx https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(original, size, bounds) {
+  console.log(original);
+  let newNum = original + Math.floor(Math.random() * size);
+  if ((newNum > bounds.w)) {
+    return bounds.w;
+  }
+  if ((newNum > bounds.h)) {
+    return bounds.h;
+  }
+  return newNum;
 }
 
-function createPolygon(points, fill) {
+
+function wobblePointPair(pointPair, size, bounds) {
+  let a = getRandomInt(pointPair[0], size, bounds),
+    b = getRandomInt(pointPair[1], size, bounds);
+  return [a, b];
+}
+
+function generatePoints(numberOfPoints, middle) {
+  let starter = [
+      [30, 30],
+      [90, 90],
+      [100, 20]
+    ],
+    resultSet = [],
+    size = 30;
+  for (var i = 0; i < numberOfPoints; i++) {
+    if (i > 0) {
+      resultSet.push([
+        wobblePointPair((resultSet[i - 1][0]), size, middle),
+        wobblePointPair((resultSet[i - 1][1]), size, middle),
+        wobblePointPair((resultSet[i - 1][2]), size, middle)
+      ]);
+    } else {
+      resultSet.push([
+        wobblePointPair((starter[0]), size, middle),
+        wobblePointPair((starter[1]), size, middle),
+        wobblePointPair((starter[2]), size, middle)
+      ]);
+    }
+  }
+  return resultSet;
+}
+
+function createPolygon(points, fill, middle) {
   let polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
   polygon.setAttribute("points", points);
   polygon.setAttribute("fill", fill);
