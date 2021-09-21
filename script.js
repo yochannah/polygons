9f785c91b842503ff354b19c4083d43f9df69d5c
Yo Yehudi <yochannah@gmail.com>
2021-09-21 21:04:07 +0100

    ooh a trinagle

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var seed = document.getElementById("seed");
+
+  // 1. get width of box
+  // 2. get center
+  // 3. build random polygon around it
+  // 4. use 2 coords to build other polygons
+  // 6. considerations:
+  // maximum number of polygons?
+  // overlaps?
+  // colours?
+
+  var middle = {
+    h: seed.height.baseVal.value,
+    w: seed.width.baseVal.value
+  };
+
+  createPolygon()
+
+});
+
+function generatePoints() {
+  return [[30,30],[90,90],[100,20]];
+}
+
+function createPolygon(points, fill) {
+  let polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+  polygon.setAttribute("points", generatePoints());
+  seed.appendChild(polygon);
+}
